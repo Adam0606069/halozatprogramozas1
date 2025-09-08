93a3def00f30f40c36ff24d73450bb91f93fe302
Adam0606069 <horadam2006@gmail.com>
2025-09-08 20:19:12 +0200

    pro

--- pro.py
+++ pro.py
@@ -24,6 +24,11 @@ nevek=["Tibi", "Sanyi", "Tibi"]
 print(f"nevek: {set(nevek)}")
 print(tuple([1,2,3]))
 
-#HF: Git parancsok
-#git init: adott mappa verzió követésének indítása +magyarázat
-#git add: 
+# HF: Git parancsok
+# git init: adott mappa verzió követésének indítása 
+# git add: Fájl hozzá adása a következő commitoláshoz
+# git clone: repository letöltése a helyi eszközre
+# git config --global user.name: felhasználónév beállítása
+# git config --global user.email: emailcím csatolása, beállítása
+# git log: commit előzmények megtekintése
+# git commit -m: commit-álni a legfrissebb munkát egy új snapshot-ként
