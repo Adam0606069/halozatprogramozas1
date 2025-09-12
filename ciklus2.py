36eff9ba19e80e9f3e509019e64c35f457a3ee39
Adam0606069 <horadam2006@gmail.com>
2025-09-12 12:58:32 +0200

    whileDone

---
+++ ciklus2.py
@@ -0,0 +1,13 @@
+#Kérjük be a számokat és adjuk meg az összegüket.
+#1. 0 végjelig
+#2. Enter végjelig HF
+
+osszeg=0
+be=1
+
+while be!=0:    
+    be=int(input("Kérek egy számot: "))
+    osszeg+=be
+
+print(osszeg)
+
