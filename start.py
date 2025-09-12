f28997be961c84ebb4adacff2c790fe0083e776f
Adam0606069 <horadam2006@gmail.com>
2025-09-12 09:42:54 +0200

    ok

--- start.py
+++ start.py
@@ -12,21 +12,27 @@
 
 def beker(alakzat, oldal):
     oldalhossz=int(input(f"Kérem a {alakzat} {oldal} oldalának hosszát [cm]: "))
+    return oldalhossz
 
-def teglalapterulete(a,b):
+def teglalapkerulete(a,b):
     '''kiszámolja az "A" és "B" oldal ismeretlen téglalap területét.'''
+    kerulet=2 * (a + b)
+    return kerulet
+
+def kiir(alakzat, kerulet):
+    print(f"A {alakzat} kerulete {kerulet}cm.")
 
 #input
 mit=input("[T]églalap vagy [N]égyzet kerületét számoljam? {T|N}?")
 
 if mit.upper() == "N":
     alap=int(input("Kérem a négyzet alapját [cm]: "))
-    kerulet=4*alap
+    kerulet=teglalapkerulete(alap, alap)
     print(f"A négyzet kerülete: {kerulet}cm")
-elif mit.upper() =="T"    
+elif mit.upper() =="T":    
     a_oldal=beker("téglalap","a")
     b_oldal=beker("téglalap","b")
-    kerulet=2*(a_oldal + b_oldal)
+    kerulet=teglalapkerulete(a_oldal, b_oldal)
     print(f"A téglalap kerülete: {kerulet}cm")
 else:
     print("Hibás választás. Kilépek!")
