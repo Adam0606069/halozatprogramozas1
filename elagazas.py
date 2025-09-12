cd309b0ac656aee904ae81027b1cad06378d6d55
Adam0606069 <horadam2006@gmail.com>
2025-09-12 10:34:30 +0200

    tesztelesek

---
+++ elagazas.py
@@ -0,0 +1,42 @@
+import random
+
+gondolt_szam = random.randint(1,3)
+# print(f"A gondolt szám {gondolt_szam}.")
+bekert_szam=int(input("Kérem a tippet: "))
+
+if gondolt_szam == bekert_szam:
+    print("Ügyes vagy, eltaláltad!")
+elif gondolt_szam > bekert_szam:
+    print("Nagyobbra gondoltam")
+else:
+    print("Kisebbre gondoltam")    
+
+# Készíts függvényt előjel néven, amely átvesz egy egész számot és az előjelét adja vissza!
+
+def elojel(szam):
+    if szam>=0:
+        return "+"
+    else:
+        return "-"
+
+szam= int(input("Kérek egy számot előjellel: "))
+
+print(f"A {szam} előjele: {elojel(szam)}.")
+
+#print (f"A {szam} előjele {'+' if szam>=0 else '-'}.")
+
+def tesztesetek():
+    teszt(5,"+")
+    teszt(-3,"-")
+    teszt(-1, "-")
+    teszt(0, "+")
+    teszt(1, "+")
+    #...
+
+def teszt(szam, elvart_elojel):
+    if elojel(szam) == elvart_elojel:
+        print(f"elojel({szam})=={elvart_elojel} true")
+    else:
+        print(f"elojel({szam})=={elvart_elojel} false")
+
+tesztesetek()
