0829e018e3831294abe4b4c67c84bb7ffcc78896
Adam0606069 <horadam2006@gmail.com>
2025-09-18 18:27:41 +0200

    enteresosszeg

--- ciklus2.py
+++ ciklus2.py
@@ -2,13 +2,25 @@
 #1. 0 végjelig
 #2. Enter végjelig HF
 
-osszeg=0
-be=1
+# osszeg=0
+# be=1
 
-while be!=0:    
-    be=int(input("Kérek egy számot: "))
-    osszeg+=be
+# while be!=0:    
+#     be=int(input("Kérek egy számot: "))
+#     osszeg+=be
 
-print(osszeg)
+# print(osszeg)
 
 # Enter végjel
+
+osszeg = 0
+
+while True:
+    be = input("Kérek egy számot: ")
+    if be == "":
+        break
+    osszeg += int(be)
+
+print("Összeg:", osszeg)
+
+#break --> Ciklus megtörése
